cbca1d765383dc08f3d0ee3e5bc0b6b04bde2664
Harshitha Tumkur Kailasa Murthy <htk@Harshithas-MBP.cable.rcn.com>
2023-11-21 00:58:20 -0500

    Changes added

--- Assignment8/main.py
+++ Assignment8/main.py
@@ -1,123 +1,81 @@
-from google.cloud import storage as storage
-from google.cloud import pubsub_v1
-from datetime import datetime, timezone
-
-import google.cloud.logging
-import os
 from flask import Flask, request, make_response
-import json
-import logging
-import sqlalchemy
-import ssl
-
+from google.cloud import storage, pubsub_v1, logging as cloud_logging
 from google.cloud import secretmanager
+import json
+import os
 
 app = Flask(__name__)
 
-pool = ""
+def is_valid_country(country):
+    unauthorized_countries = ["North Korea", "Iran", "Cuba", "Myanmar", "Iraq", "Libya", "Sudan", "Zimbabwe", "Syria"]
+    return country not in unauthorized_countries
 
-def ifExists(filesInBucket, fileName):
-    if(fileName in filesInBucket):
-        return True
-    return False
+def connect_to_cloud_logging():
+    logging_client = cloud_logging.Client()
+    logging_client.setup_logging()
+    return logging_client
 
-def connectToCloudLogging():
-    loggingClient = google.cloud.logging.Client()
-    loggingClient.setup_logging()
-    return loggingClient
-
-def getSecret(sid, vid):
-    secretClient = secretmanager.SecretManagerServiceClient()
-    name = f"projects/611183327365/secrets/{sid}/versions/{vid}"
-    response = secretClient.access_secret_version(name=name)
+def get_secret(secret_id, version_id):
+    secret_client = secretmanager.SecretManagerServiceClient()
+    secret_name = f"projects/611183327365/secrets/{secret_id}/versions/{version_id}"
+    response = secret_client.access_secret_version(name=secret_name)
     return response.payload.data.decode('UTF-8')
 
-@app.route('/<filepath>', methods = ["POST","GET","PUT","PATCH","DELETE","OPTIONS"])
-def cds561_hhtpRequest(filepath):
+@app.route('/<filepath>', methods=["POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS"])
+def handle_request(filepath):
     os.environ["ZONE"] = ""
 
-    if(request.headers.get("X-country") in ["North Korea", "Iran", "Cuba", "Myanmar", "Iraq", "Libya", "Sudan", "Zimbabwe", "Syria"]):
-        pubClient = googlePubSubConnection()
-        pub_payload = {"country" : str(request.headers.get("X-country")),
-                    "request": str(request.method),
-                    "args": str(request.args),
-                    "data": str(request.data),
-                    "message": "Request from an unauthorized country"}
-        publishMsg(pubClient, pub_payload)
-        res = make_response("Permission Denied - Unauthorized Country", 400)
-        res.headers['X-zone'] = os.environ["ZONE"]
-        return res
-    
-    loggingClient = connectToCloudLogging()
-    currLog = {
-        "httpRequest":{
-            "requestMethod": request.method
-        },
+    country_header = request.headers.get("X-country")
+    if is_valid_country(country_header):
+        notify_unauthorized_request(country_header)
+        response = make_response("Permission Denied - Unauthorized Country", 400)
+        response.headers['X-zone'] = os.environ["ZONE"]
+        return response
+
+    logging_client = connect_to_cloud_logging()
+    current_log = {
+        "httpRequest": {"requestMethod": request.method},
         "severity": "",
         "message": "",
         "statusCode": 000
     }
 
     if request.method == "GET":
+        storage_client = storage.Client.create_anonymous_client()
+        storage_bucket, files_in_bucket = get_storage_connection(storage_client, "cs561-assignment2-storage-bucket")
+        file_name = "files/" + request.path.split("/")[-1]
 
-        storageClient = connectToGoogle()
-        storageBucket, filesInBucket = sbConnection(storageClient, "cs561-assignment2-storage-bucket")
-        fileName = "files/" + request.path.split("/")[-1]
+        if not does_file_exist(files_in_bucket, file_name):
+            current_log["severity"] = "ERROR"
+            current_log["message"] = "User attempted to access non-existent file: " + file_name
+            current_log["statusCode"] = 404
+            log_warning(logging_client, current_log)
+            response = make_response("File Not Found", 404)
+            response.headers['X-zone'] = os.environ["ZONE"]
+            return response
 
-        if(not ifExists(filesInBucket, fileName)):
-            currLog["severity"] = "ERROR"
-            currLog["message"] = "User tried to search for non existant file: " + fileName
-            currLog["statusCode"] = 404
-            logging.warning(currLog)
-            res = make_response("File Not Found", 404)
-            res.headers['X-zone'] = os.environ["ZONE"]
-            return res
-
-        res = make_response(readFileFromStorage(storageBucket, fileName), 200)
-        res.headers['X-zone'] = os.environ["ZONE"]
-        return res
+        response = make_response(read_file(storage_bucket, file_name), 200)
+        response.headers['X-zone'] = os.environ["ZONE"]
+        return response
     else:
-        currLog["severity"] = "INTERNAL SERVER ERROR"
-        currLog["message"] = "Not Implemented method call : " + request.method
-        currLog["statusCode"] = 501
-        logging.warning(currLog)
-        res = make_response("Not Implemented yet", 501)
-        res.headers['X-zone'] = os.environ["ZONE"]
-        return res
+        current_log["severity"] = "INTERNAL SERVER ERROR"
+        current_log["message"] = "Not Implemented method call: " + request.method
+        current_log["statusCode"] = 501
+        log_warning(logging_client, current_log)
+        response = make_response("Not Implemented yet", 501)
+        response.headers['X-zone'] = os.environ["ZONE"]
+        return response
 
-def sbConnection(storageClient, storageBucketName):
-    storageBucket = storageClient.bucket(storageBucketName)
-    filesInBucket = [blob.name for blob in storageBucket.list_blobs()]
+def get_storage_connection(storage_client, storage_bucket_name):
+    storage_bucket = storage_client.bucket(storage_bucket_name)
+    files_in_bucket = [blob.name for blob in storage_bucket.list_blobs()]
+    return storage_bucket, files_in_bucket
 
-    return storageBucket, filesInBucket
+def does_file_exist(files_in_bucket, file_name):
+    return file_name in files_in_bucket
 
-def googlePubSubConnection():
-    pubClient = pubsub_v1.PublisherClient() 
-    return pubClient
-
-def readFileFromStorage(storageBucket, blobName):
-    blob = storageBucket.blob(blobName)
-    fileContent = ""
-
-    with blob.open("r") as f:
-        fileContent = f.read()
-    
-    return fileContent
-
-def connectToGoogle():
-    storageClient = storage.Client.create_anonymous_client()
-    return storageClient
-    
-def publishMsg(pubClient, payload):
-    PUB_SUB_TOPIC = "cds561_Assignment3"
-    PUB_SUB_PROJECT = "quiet-sanctuary-399018"
-
-    topicPath = pubClient.topic_path(PUB_SUB_PROJECT, PUB_SUB_TOPIC)        
-    jsonData = json.dumps(payload).encode("utf-8")           
-    future = pubClient.publish(topicPath, data=jsonData)
-    print("Pushed message to topic.")   
-    return
-
+def log_warning(logging_client, log_data):
+    logging_client.warning(log_data)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
